8294a32ddb3d74fe60e3cb436aaae033a2b6a279
milanevn <dothithanhthuy10ta1@gmail.com>
2026-04-09 21:48:22 +0700

    feat(security): implement SecurityFilterChain and authorization rules
    - Enable Spring Security 3 configurations
    - Permit all requests to /api/auth/**
    - Disable CSRF for REST API development

--- backend/src/main/java/com/cosmetics/ecommerce/config/SecurityConfig.java
+++ backend/src/main/java/com/cosmetics/ecommerce/config/SecurityConfig.java
@@ -2,13 +2,30 @@ package com.cosmetics.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    //1. Khai báo bộ mã hóa mật khẩu
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    //2. Cấu hình phân quyền truy cập
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
+        http.csrf(csrf -> csrf.disable()) // tắt csrf để gọi API t React/Postman không bị chặn
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasAuthority("ADMIN") //Chỉ Admin mới vào được /admin
+                        .anyRequest().authenticated() //Các request khác phải đăng nhập
+                );
+        return http.build();
     }
 }
