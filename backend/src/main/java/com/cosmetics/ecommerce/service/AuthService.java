745972815ffeda2312ee2083d5f090a5a097b10b
milanevn <dothithanhthuy10ta1@gmail.com>
2026-04-10 15:46:25 +0700

    finalize login and registration logic with JWT integration

--- backend/src/main/java/com/cosmetics/ecommerce/service/AuthService.java
+++ backend/src/main/java/com/cosmetics/ecommerce/service/AuthService.java
@@ -7,6 +7,7 @@ import com.cosmetics.ecommerce.entity.Role;
 import com.cosmetics.ecommerce.entity.User;
 import com.cosmetics.ecommerce.repository.RoleRepository;
 import com.cosmetics.ecommerce.repository.UserRepository;
+import com.cosmetics.ecommerce.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ public class AuthService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     // UC3.5 - Dang ky tai khoan (cho khach hang)
     public AuthResponse register(RegisterRequest request){
@@ -50,21 +52,30 @@ public class AuthService {
 
     // dang nhap Admin - dang nhap Customer
     public AuthResponse login (LoginRequest request){
-//        User user = userRepository.findByEmail(request.getEmail())
-//                .orElseThrow(() -> new RuntimeException("Email hoac mat khau khong chinh xac"));
-//
-//        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-//            throw new RuntimeException("Email hoac mat khau khong chinh xac");
-//        }
-//
-//        if(!user.getIsActive()){
-//            throw new RuntimeException("Tai khoan cua ban da bi khoa");
-//        }
-//
-//        String role = user.getRole().getRoleName();
-//        String token = jwtUtil.generateToken(user.getEmail(), role);
-//        return new AuthResponse(token, role, user.getName());
-        return null;
+        //tìm user bằng email
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Email hoac mat khau khong chinh xac"));
+
+        //so khớp mật khẩu đã băm
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Email hoac mat khau khong chinh xac");
+        }
+
+        //kiểm tra tài khoản có bị khóa không
+        if(!user.getIsActive()){
+            throw new RuntimeException("Tai khoan da bi khoa");
+        }
+
+        // 4. Tạo token (Sử dụng class JwtService bạn sắp viết)
+        String token = jwtUtil.generateToken(user.getEmail(), user.getRole().getRoleName());
+
+        // 5. Trả về thông tin cho Frontend
+        return AuthResponse.builder()
+                .token(token)
+                .role(user.getRole().getRoleName())
+                .name(user.getName())
+                .message("Đăng nhập thành công")
+                .build();
     }
 
 }
