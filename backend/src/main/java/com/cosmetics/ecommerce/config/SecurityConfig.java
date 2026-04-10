37b4b8e53bf8458ad45225d126e51469e2d5dd2a
milanevn <dothithanhthuy10ta1@gmail.com>
2026-04-10 16:18:21 +0700

    feat(config): activate JWT authentication flow in SecurityConfig

--- backend/src/main/java/com/cosmetics/ecommerce/config/SecurityConfig.java
+++ backend/src/main/java/com/cosmetics/ecommerce/config/SecurityConfig.java
@@ -1,31 +1,37 @@
 package com.cosmetics.ecommerce.config;
 
+import com.cosmetics.ecommerce.filter.JwtFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    //1. Khai báo bộ mã hóa mật khẩu
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+    private final JwtFilter jwtFilter;
 
-    //2. Cấu hình phân quyền truy cập
+    // Cấu hình phân quyền truy cập
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
         http.csrf(csrf -> csrf.disable()) // tắt csrf để gọi API t React/Postman không bị chặn
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/admin/**").hasAuthority("ADMIN") //Chỉ Admin mới vào được /admin
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN") //Chỉ Admin mới vào được /admin
                         .anyRequest().authenticated() //Các request khác phải đăng nhập
-                );
+                )
+                //Thêm filter của bạn vào trước bộ lọc mặc định của Spring
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
