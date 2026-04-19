df3cb2a4bd5441f2272f2ab724b781fae8701849
milanevn <dothithanhthuy10ta1@gmail.com>
2026-04-19 21:21:51 +0700

    backend/src/main/java/com/cosmetics/ecommerce/service/UserServiceLegacy.java

--- backend/src/main/java/com/cosmetics/ecommerce/service/UserServiceLegacy.java
+++ backend/src/main/java/com/cosmetics/ecommerce/service/UserServiceLegacy.java
@@ -5,15 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.cosmetics.ecommerce.entity.User;
 import com.cosmetics.ecommerce.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserServiceLegacy {
 
     private final UserRepository userRepository;
 
