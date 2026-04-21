8274fee6a6b98b7a84bdcb0b7980f16a29f08d8d
milanevn <dothithanhthuy10ta1@gmail.com>
2026-04-21 21:46:30 +0700

    backend/src/main/java/com/cosmetics/ecommerce/repository/OrderRepository.java

--- backend/src/main/java/com/cosmetics/ecommerce/repository/OrderRepository.java
+++ backend/src/main/java/com/cosmetics/ecommerce/repository/OrderRepository.java
@@ -2,11 +2,14 @@ package com.cosmetics.ecommerce.repository;
 
 import java.util.List;
 
+import com.cosmetics.ecommerce.dto.RevenueChartDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import com.cosmetics.ecommerce.entity.Order;
 import com.cosmetics.ecommerce.entity.User;
+import org.springframework.data.repository.query.Param;
+
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer>{
@@ -28,5 +31,18 @@ public interface OrderRepository extends JpaRepository<Order, Integer>{
     //Đếm số đơn hàng theo trạng thái (ví dụ: PENDING, DELIVERD, CANCELLED)
     //giúp admin biết có bao nhiêu đơn đang xử lý
     Long countByStatus(String status);
+
+    //1. Thống kê theo ngày trong tháng (Dùng cho UC3.8 - Biểu đồ doanh thu tháng)
+    @Query("SELECT new com.cosmetics.ecommerce.dto.RevenueChartDTO(DAY(o.createdAt), SUM(o.totalPrice)) " +
+            "FROM Order o WHERE o.status = 'COMPLETED' " +
+            "AND MONTH(o.createdAt) = :m AND YEAR(o.createdAt) = :y " +
+            "GROUP BY DAY(o.createdAt) ORDER BY DAY(o.createdAt)")
+    List<RevenueChartDTO> getRevenueByDay(@Param("m") int month, @Param("y") int year);
+
+    //2. Thống kê theo tháng trong năm
+    @Query("SELECT new com.cosmetics.ecommerce.dto.RevenueChartDTO(MONTH(o.createdAt), SUM(o.totalPrice)) " +
+            "FROM Order o WHERE o.status = 'COMPLETED' AND YEAR(o.createdAt) = :y " +
+            "GROUP BY MONTH(o.createdAt) ORDER BY MONTH(o.createdAt)")
+    List<RevenueChartDTO> getRevenueByMonth(@Param("y") int year);
 
 }
