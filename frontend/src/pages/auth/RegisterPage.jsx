1f80580ef14829b85acfbcd280b3357c8aaebe6d
ngthanhhhh <nguyetthanh225@gmail.com>
2026-05-12 01:33:53 +0700

    fix Thuy register and change password pages

--- frontend/src/pages/auth/RegisterPage.jsx
+++ frontend/src/pages/auth/RegisterPage.jsx
@@ -1,5 +1,163 @@
+import React, { useState } from "react";
+import {Link, useNavigate} from 'react-router-dom';
+import { registerUser } from "../../features/auth/authService";
+import "./RegisterPage.css";
+
 function RegisterPage() {
-    return <div>Register Page</div>;
+    
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        password: "",
+        confirmPassword:"",
+    });
+    // State lưu thông báo lỗi nếu có
+    const [error, setError]= useState("");
+
+    // State loading
+    const [loading, setLoading] = useState(false);
+
+    // Hàm cập nhật state mỗi khi người dùng gõ vào ô input
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    };
+
+    // Hàm xử lý khi nhấn nút "Đăng ký"
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if(formData.password.length < 6){
+            setError("Mật khẩu có ít nhất 6 ký tự");
+            return;
+        }
+
+        if(formData.password !== formData.confirmPassword){
+            setError("Mật khẩu xác nhận không khớp");
+            return;
+        }
+
+        // console.log("Du lieu hien tai: ", formData);
+        setError("");
+
+        // Đóng gói dữ liệu: Kết hợp thông tin người dùng nhập + dữ liệu mặc định hệ thống cần
+        const dataToSubmit = {
+            name: formData.name.trim(),
+            email: formData.email.trim().toLowerCase(),
+            phone: formData.phone.trim(),
+            password: formData.password,
+            confirmPassword: formData.confirmPassword, //phải có để xác nhận!!!
+            address: "",
+        };
+
+        try{
+            setLoading(true);
+            await registerUser(dataToSubmit);
+            alert("Dang ky thanh cong!");
+            navigate("/auth/login"); //Chuyen huong sau khi thanh cong
+        } catch (err){
+            setError(err.response?.data?.message || err.message || "Có lỗi xảy ra, vui lòng thử lại!");
+        } finally{
+            setLoading(false);
+        }
+
+    };
+    
+    return (
+        <div className="register-container">
+           
+            <h2 className="register-title">Tạo tài khoản</h2>
+
+            {error && <p className="error-message" style={{color: 'red', marginBottom: '10px'}}>{error}</p>}
+            
+            <form className="register-form" onSubmit={handleSubmit}>
+
+                {/* Phần nhập họ tên */}
+                <div className="form-group">
+                    <label>Họ tên</label>
+                    <input 
+                        name="name"
+                        type="text" 
+                        placeholder="Nhập họ và tên"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required></input>
+
+                </div>
+
+                {/* Phần nhập email */}
+                <div className="form-group">
+                    <label>Email</label>
+                    <input 
+                        name="email"
+                        type="email" 
+                        placeholder="example@gmail.com"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required></input>
+
+                </div>
+
+                {/* Phần nhập sdt */}
+                <div className="form-group">
+                    <label>Số điện thoại</label>
+                    <input 
+                        name="phone"
+                        type="text" 
+                        placeholder="Nhập số điện thoại"
+                        value={formData.phone}
+                        onChange={handleChange}
+                        required></input>
+
+                </div>
+                
+
+                {/* Phần nhập mật khẩu */}
+                <div className="form-group">
+                    <label>Mật khẩu</label>
+                    <input 
+                        name="password"
+                        type="password" 
+                        placeholder="Tối thiểu 6 kí tự"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required></input>
+
+                </div>
+
+                {/* Phần xác nhận mật khẩu */}
+                <div className="form-group">
+                    <label>Xác nhận mật khẩu</label>
+                    <input
+                        name="confirmPassword"
+                        type="password"
+                        placeholder="Nhập lại mật khẩu"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        required>
+
+                    </input>
+                </div>
+                
+                {/* Nút gửi form */}
+                <button 
+                    type="submit" 
+                    className="btn-submit"
+                    disabled={loading}
+                    >
+                        {loading ? "Đang đăng ký..." : "Đăng ký"}
+                </button>
+            </form>
+
+            {/* Link chuyển hướng trang đăng nhập */}
+            <div className="register-footer">
+                <span>Bạn đã có tài khoản?  </span>
+                <Link to="/auth/login">Đăng nhập!</Link>
+            </div>
+            
+        </div>
+    );
 }
 
 export default RegisterPage;
--- frontend/src/pages/customer/ChangePasswordPage.jsx
+++ frontend/src/pages/customer/ChangePasswordPage.jsx
@@ -1,5 +1,185 @@
+import { useState } from "react";
+//import { useNavigate } from "react-router-dom";
+import { changePassword } from "../../features/users/userService";
+import "./ChangePasswordPage.css";
+
+
 function ChangePasswordPage() {
-    return <div>Change Password Page</div>;
+
+    //const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        oldPassword: "",
+        newPassword: "",
+        confirmPassword: "",
+    });
+
+    const [loading, setLoading ] = useState(false);
+
+    const [error, setError] = useState("");
+
+    const [success, setSuccess] = useState("");
+
+    const handleChange = (e) => {
+
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+
+        setError("");
+        setSuccess("");
+    };
+
+    const handleSubmit = async (e) =>{
+        
+        e.preventDefault();
+
+        // Validdate mật khẩu hiện tại rỗng
+        if(!formData.oldPassword.trim()){
+            setError("Vui lòng nhập mật khẩu hiện tại");
+            return;
+        }
+
+        //Validate mật khẩu mới
+        if(formData.newPassword.length < 6){
+
+            setError("Mật khẩu mới phải có ít nhất 6 ký tự");
+
+            return;
+        }
+
+        if(formData.oldPassword === formData.newPassword){
+            setError("Mật khẩu mới phải khác mật khẩu cũ");
+            return;
+        }
+
+        //Validate mật khẩu xác nhận
+        if(
+            formData.newPassword !==
+            formData.confirmPassword
+        ) {
+            setError("Mật khẩu xác nhận không khớp");
+
+            return;
+        }
+
+        try{
+            setLoading(true);
+
+            await changePassword({
+                oldPassword: formData.oldPassword,
+                newPassword: formData.newPassword,
+                confirmPassword: formData.confirmPassword
+            });
+
+            setSuccess("Đổi mật khẩu thành công!");
+
+            setFormData({
+                oldPassword: "",
+                newPassword: "",
+                confirmPassword: "",
+            });
+
+            //navigate("profile");
+        } catch(err){
+
+            setError(
+                err.response?.data?.message ||
+                "Đổi mật khẩu thất bại"
+            );
+        } finally {
+            setLoading(false);
+        }
+    };
+    
+    return (
+
+        <div className="change-pw-container">
+
+            <h2 className="change-pw-title">Đổi mật khẩu</h2>
+
+            {error && (
+                <p
+                    style={{
+                        color: "red",
+                        marginBottom: "10px",
+                    }}
+                >
+                    {error}
+                </p>
+            )}
+
+            {success && (
+                <p
+                    style={{
+                        color: "green",
+                        marginBottom: "10px",
+                    }}
+                >
+                    {success}
+                </p>
+            )}
+
+            <form
+                className="change-pw-form"
+                onSubmit={handleSubmit}>
+
+                <div className="form-group">
+                    
+                    <label>Mật khẩu hiện tại</label>
+
+                    <input
+                        type="password"
+                        name="oldPassword"
+                        placeholder="Nhập mật khẩu hiện tại"
+                        value={formData.oldPassword}
+                        onChange={handleChange}
+                        required>
+
+                    </input>
+                </div>
+
+                <div className="form-group">
+                    <label>Mật khẩu mới</label>
+
+                    <input
+                        type="password"
+                        name="newPassword"
+                        placeholder="Nhập mật khẩu mới"
+                        value={formData.newPassword}
+                        onChange={handleChange}
+                        required>
+                        
+                    </input>
+
+                </div>
+
+                <div className="form-group">
+                    <label>Xác nhận mật khẩu mới</label>
+
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        placeholder="Nhập lại mật khẩu mới"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        required>
+                        
+                    </input>
+
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn-submit"
+                    disabled={loading}>
+                        {loading ? "Đang xử lý..." : "Đổi mật khẩu"}
+                </button>
+            </form>
+        </div>
+
+    );
 }
 
 export default ChangePasswordPage;
